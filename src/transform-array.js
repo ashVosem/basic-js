b155afb9a40126dd5e3983bc87efd00dd3143b6c
AshVosem <ash.vosem@gmail.com>
2020-03-04 06:33:35 +0300

    feat: vigenere-cipher

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,39 +1,26 @@
 module.exports = function transform(arr) {
+    const flags = {
+        1 : '--double-next',
+        2 : '--double-prev',
+        3 : '--discard-prev',
+        4 : '--discard-next'
+    }
+
     arr.flat(1).forEach((elem, index, arr) => {
-        if(elem === '--discard-next' || elem === '--double-next' 
-        || elem === '--discard-prev' ||  elem === '--double-prev') {
-            if(elem[index + 1] === '--discard-next' || elem[index + 1] === '--double-next' 
-                || elem[index + 1] === '--discard-prev' ||  elem[index + 1] === '--double-prev') {
-                arr.slice(index, index)
-            }
-            if(elem[index - 1] === '--discard-next' || elem[index - 1] === '--double-next' 
-                || elem[index - 1] === '--discard-prev' ||  elem[index - 1] === '--double-prev') {
-                arr.slice(index, index) 
-            }
-            if(elem[0] === '--discard-prev' ||  elem[0] === '--double-prev') {
-                arr.slice(index, index) 
-            }
-            if(elem[arr.length - 1] === '--discard-next' || elem[arr.length - 1] === '--double-next') {
-                arr.slice(index, index) 
-            }
-        }
-        if(elem === '--double-next' && elem[index + 2] === '--discard-prev') {
-            arr.slice(index, index) 
-            arr.slice(index + 2, index + 2) 
-        }
-        
-        if(elem === '--discard-next') {
+        if(elem === flags[4]) {
             arr.splice(index, 2)
         }
-        if(elem === '--discard-prev') {
+        if(elem === flags[3]) {
             arr.splice(index - 1 , 2)
         }
-        if(elem === '--double-next') {
+        if(elem === flags[2]) {
+            arr.splice(index, 1, arr[index - 1])
+        }  
+        if(elem === flags[1]) {
             arr.splice(index, 1, arr[index + 1])
         }
-        if(elem === '--double-prev') {
-            arr.splice(index, 1, arr[index - 1])
-        }    
+        
     })
+
     return arr
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,58 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(param) {
+        this.param = param
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    transform_key(text, key) {
+        let transform_key = key.repeat(Math.ceil(text.length/key.length)).split('')
+
+        for(let i = 0; i < text.length ;i++) {
+            if(text[i] === ' ') {  
+                transform_key.splice(i, 0, ' ')
+            }
+        }
+       
+        return transform_key.join('')
+    }
+
+    encrypt(text, key) {
+        if (!text || !key) throw new Error()        
+
+        let letter
+        let answer = ''
+
+        key = this.transform_key(text, key)
+        
+        for(let i = 0; i < text.length; i++) {
+            if(/[a-zA-Z]/.test(text[i])) {
+                letter = (text[i].toUpperCase().charCodeAt() - 65 + key[i].toUpperCase().charCodeAt() - 65)%26 + 65
+                answer = answer.concat(String.fromCharCode(letter))
+            }
+            else answer = answer.concat(text[i])
+            
+        }
+        return this.param === undefined ? answer : answer.split('').reverse().join('')
+    }
+
+    decrypt(text, key) {
+        if (!text || !key) throw new Error()        
+
+        let letter
+        let answer = ''
+
+        key = this.transform_key(text, key)
+        
+        for(let i = 0; i < text.length; i++) {
+            if(/[a-zA-Z]/.test(text[i])) {
+                letter = (text[i].toUpperCase().charCodeAt() - 65 - key[i].toUpperCase().charCodeAt() - 65)%26
+                if(letter < 0) {
+                    letter +=26
+                }
+                answer = answer.concat(String.fromCharCode(letter + 65))
+            }
+            else answer = answer.concat(text[i])
+        }
+        return this.param === undefined ? answer : answer.split('').reverse().join('')
     }
 }
 
